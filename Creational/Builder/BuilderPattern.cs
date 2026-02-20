b0c777decbc57ab8090eccb849a51f377a502409
Saitejaz <saitejamudhulkar9@gmail.com>
2026-02-20 23:33:01 +0530

    BuilderCDP

---
+++ Creational/Builder/BuilderPattern.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace BuilderPattern
+{
+    public class BuilderPattern
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Builder Pattern");
+        }
+    }
+}
---
+++ Creational/Builder/BuilderPattern.java
@@ -0,0 +1,5 @@
+package Creational.Builder;
+
+public class BuilderPattern {
+
+}
